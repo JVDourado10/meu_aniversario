6d7397552db1a8643089e153002bb4cb130f1401
JVDourado10 <jovneiva@gmail.com>
2023-07-11 11:41:46 -0300

    add: conteudo do site

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -574,6 +574,26 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"3cYfC":[function(require,module,exports) {
+AOS.init();
+const dataDoEvento = new Date("May 27, 2024 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+    const diaEmMs = 86400000;
+    const horaEmMs = 3600000;
+    const minEmMs = 60000;
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor(distanciaAteOEvento % diaEmMs / horaEmMs);
+    const minutosAteOEvento = Math.floor(distanciaAteOEvento % horaEmMs / minEmMs);
+    const segundosAteOEvento = Math.floor(distanciaAteOEvento % minEmMs / 1000);
+    document.getElementById("header__date").innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById("header__date").innerHTML = "Evento Expirado";
+    }
+}, 1000);
 botao = document.querySelector(".header__infos__button");
 botao.addEventListener("mouseover", function() {
     botao.innerHTML = "Manda no Whats :D";
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
+AOS.init();
+
+const dataDoEvento = new Date("May 27, 2024 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function () {
+  const agora = new Date();
+  const timeStampAtual = agora.getTime();
+
+  const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+  const diaEmMs = 1000 * 60 * 60 * 24;
+  const horaEmMs = 1000 * 60 * 60;
+  const minEmMs = 1000 * 60;
+
+  const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+  const horasAteOEvento = Math.floor(
+    (distanciaAteOEvento % diaEmMs) / horaEmMs
+  );
+  const minutosAteOEvento = Math.floor(
+    (distanciaAteOEvento % horaEmMs) / minEmMs
+  );
+  const segundosAteOEvento = Math.floor((distanciaAteOEvento % minEmMs) / 1000);
+
+  document.getElementById(
+    "header__date"
+  ).innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+  if (distanciaAteOEvento < 0) {
+    clearInterval(contaAsHoras);
+    document.getElementById("header__date").innerHTML = "Evento Expirado";
+  }
+}, 1000);
+
 botao = document.querySelector(".header__infos__button");
 
 botao.addEventListener("mouseover", function () {
